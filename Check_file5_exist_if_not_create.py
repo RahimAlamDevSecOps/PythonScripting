cbdbb824e90d9490133c396a187307a6cfea04f9
Rahim <rahimalam188@gmail.com>
2026-06-28 06:43:41 +0000

    Modified

--- Check_file5_exist_if_not_create.py
+++ Check_file5_exist_if_not_create.py
@@ -1,8 +1,12 @@
 import os
-filename = "file7"
-if os.path.exists(filename):
-    print(f"{filename} is exist.")
-else:
-    print(f"{filename} does not exists. Creating {filename}...")
-    with open(filename, "w") as file:
-        print(f"{filename} created")
+filename = "file5"
+try:
+    if os.path.exists(filename):
+        print(f"{filename} already exists.")
+    else:
+        print(f"{filename} does not exist. Creating ....!")
+        with open(filename, "w") as file:
+            file.write("Hello Rahim")
+            print(f"{filename} created successfully")
+except Exception as e:
+    print(f"An error occured as: {e}")
